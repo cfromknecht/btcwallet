6dd885a87c900535c482794b14cc1c1637d22c79
Conner Fromknecht <conner@mit.edu>
2018-03-20 18:04:15 -0700

    chain/rpc: impl FilterBlocks via filters + rescan

--- chain/rpc.go
+++ chain/rpc.go
@@ -13,7 +13,10 @@ import (
 	"github.com/roasbeef/btcd/chaincfg"
 	"github.com/roasbeef/btcd/chaincfg/chainhash"
 	"github.com/roasbeef/btcd/rpcclient"
+	"github.com/roasbeef/btcd/wire"
 	"github.com/roasbeef/btcutil"
+	"github.com/roasbeef/btcutil/gcs"
+	"github.com/roasbeef/btcutil/gcs/builder"
 	"github.com/roasbeef/btcwallet/waddrmgr"
 	"github.com/roasbeef/btcwallet/wtxmgr"
 )
@@ -161,6 +164,85 @@ func (c *RPCClient) BlockStamp() (*waddrmgr.BlockStamp, error) {
 	case <-c.quit:
 		return nil, errors.New("disconnected")
 	}
+}
+
+func (c *RPCClient) FilterBlocks(
+	req *FilterBlocksRequest) (*FilterBlocksResponse, error) {
+
+	blockFilterer := NewBlockFilterer(
+		c.chainParams, req.ExternalAddrs, req.InternalAddrs,
+	)
+
+	nAddrs := len(req.ExternalAddrs) + len(req.InternalAddrs)
+	watchList := make([][]byte, nAddrs)
+	for _, addr := range req.ExternalAddrs {
+		watchList = append(watchList, addr.ScriptAddress())
+	}
+	for _, addr := range req.InternalAddrs {
+		watchList = append(watchList, addr.ScriptAddress())
+	}
+
+	// Iterate over the requested blocks, fetching each from the rpc client.
+	// Each block will scanned using the reverse addresses indexes generated
+	// above, breaking out early if any addresses are found.
+	for i, blk := range req.Blocks {
+		rawFilter, err := c.GetCFilter(&blk.Hash, wire.GCSFilterRegular)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(rawFilter.Data) < 4 {
+			log.Infof("filter data too small at height=%d", blk.Height)
+			continue
+		}
+
+		filter, err := gcs.FromNBytes(builder.DefaultP, rawFilter.Data)
+		if err != nil {
+			return nil, err
+		}
+
+		if filter.N() == 0 {
+			log.Infof("empty filter at height=%d", blk.Height)
+			continue
+		}
+
+		key := builder.DeriveKey(&blk.Hash)
+		matched, err := filter.MatchAny(key, watchList)
+		if err != nil {
+			return nil, err
+		} else if !matched {
+			continue
+		}
+
+		log.Infof("matched block height=%d", blk.Height)
+
+		rawBlock, err := c.GetBlock(&blk.Hash)
+		if err != nil {
+			return nil, err
+		}
+
+		if !blockFilterer.FilterBlock(rawBlock) {
+			continue
+		}
+
+		// If any external or internal addresses were detected in this
+		// block, we return them to the caller so that the rescan
+		// windows can widened with subsequent addresses. The
+		// `BatchIndex` is returned so that the caller can compute the
+		// *next* block from which to begin again.
+		resp := &FilterBlocksResponse{
+			BatchIndex:         uint32(i),
+			BlockMeta:          blk,
+			FoundExternalAddrs: blockFilterer.FoundExternal,
+			FoundInternalAddrs: blockFilterer.FoundInternal,
+			RelevantTxns:       blockFilterer.RelevantTxns,
+		}
+
+		return resp, nil
+	}
+
+	// No addresses were found for this range.
+	return nil, nil
 }
 
 // parseBlock parses a btcws definition of the block a tx is mined it to the
